454763b8da202435e5bdfdf167590fd538996ca3
Cepr0 <cepr0@ukr.net>
2017-07-27 20:09:32 +0300

    still playing...

--- src/main/java/io/github/cepr0/springdto/rest/CategoryController.java
+++ src/main/java/io/github/cepr0/springdto/rest/CategoryController.java
@@ -1,5 +1,6 @@
 package io.github.cepr0.springdto.rest;
 
+import io.github.cepr0.springdto.domain.Category;
 import io.github.cepr0.springdto.dto.CategoryClassDto;
 import io.github.cepr0.springdto.dto.CategoryInterfaceDto;
 import io.github.cepr0.springdto.repo.CategoryRepo;
@@ -8,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
 import org.springframework.hateoas.Link;
-import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,8 +34,6 @@ public class CategoryController {
     @GetMapping("/classDto")
     public ResponseEntity<?> classDto() {
         List<CategoryClassDto> dtos = repo.getClassDtos();
-        return ResponseEntity.ok(new Resources<>(dtos));
-        List<ClassDto> dtos = repo.getClassDtos();
 
         Link selfRel = entityLinks.linkFor(Category.class).slash("/classDto").withSelfRel();
         return ResponseEntity.ok(new Resources<>(dtos, selfRel));
